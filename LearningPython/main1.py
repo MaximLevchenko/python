d03211bc34f734532044b41d37910dcb6456967d
User win 10 <levchenko.maksym@gmz31.com>
2020-07-15 13:48:33 +0300

    recent

--- LearningPython/main1.py
+++ LearningPython/main1.py
@@ -1,17 +1,17 @@
-from selenium import webdriver
 from time import sleep
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
+# driver = webdriver.Chrome(ChromeDriverManager().install())
 
-
-
-
+#'//button[contains(text()."Не сейчас")]'
 
 
 class InstaBot:
 
     def __init__(self, username, password):
         self.driver = webdriver.Chrome()
+
         self.driver.get('https://www.instagram.com/?hl=ru')
         sleep(2)
         self.driver.find_element_by_xpath("//input[@name=\"username\"]") \
@@ -21,7 +21,12 @@ class InstaBot:
         self.driver.find_element_by_xpath('//button[@type="submit"]') \
             .click()
         sleep(4)
-        self.driver.find_element_by_xpath('//button[contains(text()."Не сейчас")]').click()
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/div/button').click()
+        sleep(4)
+        self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+        sleep(3)
+        self.driver.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[2]/a/svg').click()
+        sleep(10)
 
 
 InstaBot('_max_leva_', '447781470659')

b6b2c3228dfe589b2d057018eb3426026f24d54d
User win 10 <levchenko.maksym@gmz31.com>
2020-07-16 02:00:29 +0300

    recent

--- LearningPython/main1.py
+++ LearningPython/main1.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.keys import Keys
 class InstaBot:
 
     def __init__(self, username, password):
+        self.username = username;
         self.driver = webdriver.Chrome()
 
         self.driver.get('https://www.instagram.com/?hl=ru')
@@ -17,18 +18,51 @@ class InstaBot:
         self.driver.find_element_by_xpath('//button[@type="submit"]') \
             .click()
         sleep(4)
-        self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/div/button').click()
+        self.driver.find_element_by_xpath(
+            '/html/body/div[1]/section/main/div/div/div/div/button').click()  # Notifications
         sleep(2)
-        self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+        self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()  # Notifications
         sleep(2)
-        self.driver.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[5]/span').click()
+        self.driver.find_element_by_xpath(
+            '/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[5]/span').click()  # Profile
         sleep(2)
 
         self.driver.find_element_by_xpath(
-            '/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[5]/div[2]/div/div[2]/div[2]/a[1]/div/div[2]/div/div/div/div').click()
+            '/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[5]/div[2]/div/div[2]/div[2]/a[1]/div/div[2]/div/div/div/div').click()  # Profile
         sleep(2)
-        self.driver.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[2]/a').click()
-        sleep(4)
+        followers = self.followers()
+
+    def followers(self):
+        sleep(2)
+        self.driver.find_element_by_xpath('//a[@href="/_max_leva_/followers/"]').click()  # Followers list
+
+        sleep(2)
+        scroll_box = self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div[2]")  # Scrolling body
+        last_ht, ht = 0, 1
+        while last_ht != ht:
+            last_ht = ht
+            sleep(1)
+            ht = self.driver.execute_script("""   
+                        arguments[0].scrollTo(0, arguments[0].scrollHeight); 
+                        return arguments[0].scrollHeight;
+                        """, scroll_box)
+            # Scrolling the end of the scrolling block
+        links = scroll_box.find_elements_by_tag_name('a') # Getting all user names with tag 'a'
+        names = [name.text for name in links if
+                 name.text != '']  # We are making a for loop for links and checking whether or not thee
+        sorted_names=sorted(names)
+        print(sorted_names)
+        return sorted_names
+
+    # self.driver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div/div[2]/button") \
+    # .click()
+    # return names
+    # self.driver.find_element_by_xpath('//a[@href="/net tale/"]').click()
+    # sleep(3)
+    # sugs=self.driver.find_element_by_xpath()
+
+    # self.driver.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[2]/a').click()
+    # sleep(4) #Getting into dms
 
     def open_chat(self):
         self.driver.find_element_by_link_text("net_tale").click()  # Clicking on specific person

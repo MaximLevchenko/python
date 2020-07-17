9b1e7330a20dc2e7ec53b9cd9c602fad4bd9f411
User win 10 <levchenko.maksym@gmz31.com>
2020-07-17 14:38:04 +0300

    recent

--- LearningPython/robot_add_del.py
+++ LearningPython/robot_add_del.py
@@ -1,28 +1,32 @@
 class Robot:
     population = 0
 
-    def __init__(self, name, age, strength):
+    def __init__(self, name, age, strength, isAlive):
         '''This method is called every time when the class object is realised cause it is marked by __init__'''
 
         self.name = name
         self.age_of_the_robot = age
         self.strength = strength
+        self.isAlive = isAlive
         print('(Initialization of {}, he is {} years old and has his strength is: {} '.format(self.name,
                                                                                               self.age_of_the_robot,
                                                                                               self.strength))
         Robot.population += 1
 
-    def __del__(self):
+    def __del__(self, ):
         '''This method is invoked when the class object is destroyed'''
-        print('The robot with the name {},strength: {} and age {} is destroyed'.format(self.name, self.strength,
-                                                                                       self.age_of_the_robot))
 
         if Robot.population == 0:
-            print("{} was the last robot alive".format(self.name))
+            # print("{} was the last robot alive346".format(self.name))
+            return
 
         else:
+            print('The robot with the name {},strength: {} and age {} is destroyed'.format(self.name, self.strength,
+                                                                                           self.age_of_the_robot))
             Robot.population -= 1
             print('There are {} robots left'.format(Robot.population))
+            if Robot.population == 0:
+                print("{} was the last robot alive".format(self.name))
 
     def sayHi(self):
         '''Then we enter this method cause we realise this in the class object realisation'''
@@ -43,8 +47,9 @@ class Robot:
             print(
                 "In the battle between {} and {}. {} was the winner with with the difference of strength: {}. The {} is now dead".format(
                     robot1.name, robot2.name, winner_name, difference_in_strength, loser_name))
-            loser.__del__()
 
+            loser.isAlive = False
+            del loser
 
         else:
             winner = robot2
@@ -55,22 +60,23 @@ class Robot:
             print(
                 "In the battle between {} and {}. {} was the winner with with the difference of strength: {}. The {} is now dead".format(
                     robot1.name, robot2.name, winner_name, difference_in_strength, loser_name))
-            loser.__del__()
+            loser.isAlive = False
+            del loser
 
 
-    def fegf(self):
-        while Robot.population!=0:
-            del robot1
-
-
-
-robot1 = Robot("MaxGoodRobot", 17, 70)
+robot1 = Robot("MaxGoodRobot", 17, 70, True)
 robot1.sayHi()
 robot1.howManyRobots()
-robot2 = Robot("NataliBestRobot", 17, 50)
+robot2 = Robot("NataliBestRobot", 17, 50, True)
 robot2.sayHi()
 robot2.howManyRobots()
+robot3 = Robot("NiceRobot", 15, 24, True)
+robot3.sayHi()
+robot3.howManyRobots()
+
 Robot.compare(Robot)
 print('\nHere robots can continue their job.\n')
 
 print('Robots ended their job, let`s delete them\n')
+del robot1
+del robot3

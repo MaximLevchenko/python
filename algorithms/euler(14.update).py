9930bc20453a37f74dc78c361c774ebb6184e54b
MaximLevchenko <levchenko.maksym@gmz31.com>
2020-08-07 15:56:26 +0300

    euler update

---
+++ algorithms/euler(14.update).py
@@ -0,0 +1,34 @@
+import time
+def main():
+    current_longest_chain = 0
+    number_that_produced_chain = 0
+    start=int(time.time())
+    x = 1
+    number_to_collatz_up_to = 1000000
+    while x < number_to_collatz_up_to:
+        current_number = x
+        current_chain = 1
+        while current_number != 1:
+            current_number = collatz(current_number)
+            current_chain += 1
+
+        if current_chain > current_longest_chain:
+            current_longest_chain = current_chain
+            number_that_produced_chain=x
+            print(number_that_produced_chain,current_longest_chain)
+
+        current_chain = 1
+        x += 1
+    print(number_that_produced_chain,current_longest_chain )
+
+
+def collatz(number):
+    if number % 2 == 0:
+        number = number / 2
+    else:
+        number = number * 3 + 1
+    # print(number)
+    return number
+
+
+main()

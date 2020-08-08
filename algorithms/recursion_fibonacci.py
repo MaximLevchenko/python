522c15d1e8bb75820c16c9a8f893912cf6e618b8
MaximLevchenko <levchenko.maksym@gmz31.com>
2020-08-08 19:16:43 +0300

    euler update

---
+++ algorithms/recursion_fibonacci.py
@@ -0,0 +1,21 @@
+def fib_recursive(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fib_recursive(n - 1) + fib_recursive(n - 2)
+
+
+print(fib_recursive(4))
+
+# def factorial(n):
+#    print("factorial has been called with n = " + str(n))
+#    if n == 1:
+#        return 1
+#    else:
+#        res = n * factorial(n-1)
+#        print("intermediate result for ", n, " * factorial(" ,n-1, "): ",res)
+#        return res
+#
+# print(factorial(5))
